d9f0b528a2f44c05453061cc7199856282465b4b
Stefan Nieuwenhuis <Stefan.Nieuwenhuis@ordina.nl>
2017-02-12 21:58:24 +0100

    Initial commit

---
+++ routes/api.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const API = 'http://services.arcgisonline.nl/arcgis/rest/services/Demografie/CBS_WijkenBuurten_2011/MapServer/2';
+
+router.get('/towns', (req, res) =>{
+    axios.get(`${API}/query`, {
+        f: 'pjson',
+        where: '1=1',
+        outFields: 'objectid, gm_code, gm_naam, aant_inw, oad, bev_dichth, aantal_hh, p_elek_tot, p_gas_tot',
+        orderByFields: 'gm_naam',
+        returnGeometry: false
+    })
+    .then(towns => res.status(200).json(towns.data.features))
+    .catch(error => res.status(500).send(error));
+});
+
+router.get('/towns/:id', (req, res) =>{
+    axios.get(`${API}/query`, {
+        f: 'pjson',
+        where: `objectid = ${req.params.id}`,
+        outFields: 'objectid, gm_code, gm_naam, aant_inw, oad, bev_dichth, aantal_hh, p_elek_tot, p_gas_tot',
+        orderByFields: 'gm_naam',
+        returnGeometry: false
+    })
+    .then(towns => res.status(200).json(towns.data.features))
+    .catch(error => res.status(500).send(error));
+});
+
+router.get('/towns/geo/:point', (req, res) =>{
+    axios.get(`${API}/query`, {
+        f: 'pjson',
+        where: '1=1',
+        geometry: req.params.point,
+        outFields: 'objectid, gm_code, gm_naam, aant_inw, oad, bev_dichth, aantal_hh, p_elek_tot, p_gas_tot',
+        orderByFields: 'gm_naam',
+        returnGeometry: false
+    })
+    .then(towns => res.status(200).json(towns.data.features))
+    .catch(error => res.status(500).send(error));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const api = requier('./server/routes/api');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function (req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-type, Accept');
+    next();
+});
+
+app.use('/api', api);
+
+const port = process.env.port || '8080';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () =>console.log(`Api is running on localhost:${port}`));
