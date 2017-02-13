daa228d1b5882d84d1e0524c232c75b517e0f785
Stefan Nieuwenhuis <Stefan.Nieuwenhuis@ordina.nl>
2017-02-13 09:20:27 +0100

    Api is working!

--- routes/api.js
+++ routes/api.js
@@ -1,48 +1,57 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const API = 'http://services.arcgisonline.nl/arcgis/rest/services/Demografie/CBS_WijkenBuurten_2011/MapServer/2';
+const API = 'http://services.arcgisonline.nl/arcgis/rest/services/Demografie/CBS_WijkenBuurten_2011/MapServer/2/query/';
+
+const outFields = 'objectid, gm_code, gm_naam, aant_inw, oad, bev_dichth, aantal_hh, p_elek_tot, p_gas_tot';
 
 router.get('/', (req, res) => {
     res.send('api works');
 });
 
-router.get('/towns', (req, res) =>{
-    console.log(`${API}/query`);
-    axios.get(`${API}/query?f=pjson`, {
-        where: '1=1',
-        outFields: 'objectid, gm_code, gm_naam, aant_inw, oad, bev_dichth, aantal_hh, p_elek_tot, p_gas_tot',
-        orderByFields: 'gm_naam',
-        returnGeometry: false
+router.get('/towns', (req, res) => {
+
+    axios.get(`${API}`, {
+        params: {
+            f: 'pjson',
+            where: '1=1',
+            outFields: outFields,
+            orderByFields: 'gm_naam',
+            returnGeometry: false
+        }
     })
-    .then(towns => res.status(200).json(towns.data.features))
-    .catch(error => res.status(500).send(error));
-    
+        .then(towns => res.status(200).json(towns.data.features))
+        .catch(error => res.status(500).send(error));
+
 });
 
-router.get('/towns/:id', (req, res) =>{
-    axios.get(`${API}/query`, {
-        f: 'pjson',
-        where: `objectid = ${req.params.id}`,
-        outFields: 'objectid, gm_code, gm_naam, aant_inw, oad, bev_dichth, aantal_hh, p_elek_tot, p_gas_tot',
-        orderByFields: 'gm_naam',
-        returnGeometry: false
+router.get('/towns/:id', (req, res) => {
+    axios.get(`${API}`, {
+        params: {
+            f: 'pjson',
+            where: `objectid = ${req.params.id}`,
+            outFields: 'objectid, gm_code, gm_naam, aant_inw, oad, bev_dichth, aantal_hh, p_elek_tot, p_gas_tot',
+            orderByFields: 'gm_naam',
+            returnGeometry: false
+        }
     })
-    .then(towns => res.status(200).json(towns.data.features))
-    .catch(error => res.status(500).send(error));
+        .then(towns => res.status(200).json(towns.data.features))
+        .catch(error => res.status(500).send(error));
 });
 
-router.get('/towns/geo/:point', (req, res) =>{
-    axios.get(`${API}/query`, {
-        f: 'pjson',
-        where: '1=1',
-        geometry: req.params.point,
-        outFields: 'objectid, gm_code, gm_naam, aant_inw, oad, bev_dichth, aantal_hh, p_elek_tot, p_gas_tot',
-        orderByFields: 'gm_naam',
-        returnGeometry: false
+router.get('/towns/geo/:point', (req, res) => {
+    axios.get(`${API}`, {
+        params: {
+            f: 'pjson',
+            where: '1=1',
+            geometry: req.params.point,
+            outFields: 'objectid, gm_code, gm_naam, aant_inw, oad, bev_dichth, aantal_hh, p_elek_tot, p_gas_tot',
+            orderByFields: 'gm_naam',
+            returnGeometry: false
+        }
     })
-    .then(towns => res.status(200).json(towns.data.features))
-    .catch(error => res.status(500).send(error));
+        .then(towns => res.status(200).json(towns.data.features))
+        .catch(error => res.status(500).send(error));
 });
 
 module.exports = router;
